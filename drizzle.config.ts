f5a4a8d7bb16fb9788c008050898799f285339d4
maardgs <mariliarodrigues.94@gmail.com>
2024-09-12 15:25:09 -0300

    :wrench:configurando ambiente

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit'
 import { env } from './src/env'
 
 export default defineConfig({
-  schema: './src/db/schema',
+  schema: './src/db/schema.ts',
   out: './.migrations',
   dialect: 'postgresql',
   dbCredentials: {
